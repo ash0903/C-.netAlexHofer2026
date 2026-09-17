b4df3b27c531b7fc01607479e1179123f6b17416
ash0903 <65115534+ash0903@users.noreply.github.com>
2026-09-17 15:02:23 -0600

    Add exception handling to user input program

---
+++ ExceptionDemo.cs
@@ -0,0 +1,45 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string doAnother;
+
+        do
+        {
+            try
+            {
+                Console.Write("Please enter num1: ");
+                string num1str = Console.ReadLine();
+                int num1 = int.Parse(num1str);
+
+                Console.Write("Please enter num2: ");
+                string num2str = Console.ReadLine();
+                int num2 = int.Parse(num2str);
+
+                Console.WriteLine("Num1/Num2: {0}", num1 / num2);
+            }
+            catch (System.FormatException exc)
+            {
+                Console.WriteLine("Not a number!");
+            }
+
+            catch (System.OverflowException exc)
+            {
+                Console.WriteLine("Number is too large or small!");
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine("Error! " + exc.Message + "\nException type: " + exc.GetType());
+            }
+            finally
+            {
+                Console.WriteLine("This section always executes!");
+            }
+            Console.Write("Do another (y/n): ");
+            doAnother = Console.ReadLine();
+
+        } while (doAnother == "y");
+    }
+}
